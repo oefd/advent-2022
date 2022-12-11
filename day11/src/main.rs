69445e3f608c0bcc4501b2adc62ba3a7ddc4c944
Terry Kerr <terry@oefd.net>
2022-12-11 11:48:22 -0500

    much cleaner day11 part 1

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,66 +1,47 @@
-use bigint::U512;
-
+#![feature(iter_array_chunks)]
 mod monkey;
-mod operation;
-mod test;
 
 use monkey::Monkey;
 
 fn main() {
     let stdin = util::Stdin::new();
-    let lines = stdin.cleaned_lines();
-    let mut monkies: Vec<Monkey> = parse_monkies(lines).collect();
+    let mut lines = stdin.cleaned_lines().into_iter();
+    let mut monkies: Vec<Monkey> = lines
+        .by_ref()
+        .array_chunks::<6>()
+        .map(|monkey_def| Monkey::from(&monkey_def))
+        .collect();
 
-    // for _ in 0..20 {
-    //     perform_round(&mut monkies, false);
-    // }
-
-    for round in 0..1000 {
-        if round % 1000 == 0 {
-            println!("round {}...", round);
-        }
-        perform_round(&mut monkies, true);
-    }
-    for (i, monkey) in monkies.iter().enumerate() {
-        println!("monkey {} inspected {}", i, monkey.inspections);
+    for _round in 0..20 {
+        round(&mut monkies);
     }
 
-    let most_active = monkies.iter().map(|m| &m.inspections).fold(
-        (U512::from(0), U512::from(0)),
-        |acc, inspections| {
-            if inspections > &acc.0 {
-                (inspections.clone(), acc.0)
-            } else if inspections > &acc.1 {
-                (acc.0, inspections.clone())
-            } else {
-                acc
-            }
-        },
-    );
-    let monkey_business = most_active.0 * most_active.1;
+    let top_inspections =
+        monkies
+            .iter()
+            .map(|monkey| monkey.inspections)
+            .fold((0u64, 0u64), |acc, inspections| {
+                if inspections > acc.0 {
+                    (inspections, acc.0)
+                } else if inspections > acc.1 {
+                    (acc.0, inspections)
+                } else {
+                    acc
+                }
+            });
+    let monkey_business = top_inspections.0 * top_inspections.1;
+
     println!("monkey business: {}", monkey_business);
 }
 
-fn perform_round(monkies: &mut Vec<Monkey>, round_2: bool) {
+fn round(monkies: &mut Vec<Monkey>) {
     for i in 0..monkies.len() {
         loop {
-            if let Some((item, thrown_to)) = monkies[i].inspect(round_2) {
-                monkies[thrown_to].items.push_back(item);
+            if let Some(throw) = monkies[i].inspect_item() {
+                monkies[throw.1].items.push_back(throw.0);
             } else {
                 break;
             }
         }
     }
-}
-
-fn parse_monkies(iter: impl Iterator<Item = String>) -> impl Iterator<Item = Monkey> {
-    let mut iter = iter.peekable();
-    std::iter::from_fn(move || {
-        if iter.peek().is_some() {
-            let iter: &mut dyn Iterator<Item = String> = &mut iter;
-            Some(Monkey::from(iter))
-        } else {
-            None
-        }
-    })
 }
--- day11/src/monkey.rs
+++ day11/src/monkey.rs
@@ -1,81 +1,84 @@
-use bigint::U512;
 use std::collections::VecDeque;
 
-use crate::{operation, test};
-
 pub struct Monkey {
-    pub items: VecDeque<U512>,
-    pub inspections: U512,
-    op: Box<dyn Fn(&U512) -> U512>,
-    test: Box<dyn Fn(&U512) -> bool>,
-    throw_true: usize,
-    throw_false: usize,
-}
-
-impl From<&mut dyn Iterator<Item = String>> for Monkey {
-    fn from(iter: &mut dyn Iterator<Item = String>) -> Self {
-        let _ = iter.next().unwrap(); // drop `Monkey n:` line
-
-        let items = iter.next().unwrap();
-        assert!(items.starts_with("  Starting items: "));
-        let items = &items[18..];
-        let items: VecDeque<U512> = items
-            .split(", ")
-            .map(|item| item.parse::<u64>().unwrap().into())
-            .collect();
-
-        let op = iter.next().unwrap();
-        assert!(op.starts_with("  Operation: "));
-        let op = &op[13..];
-        let op = operation::parse(op);
-
-        let test = iter.next().unwrap();
-        assert!(test.starts_with("  Test: "));
-        let test = &test[8..];
-        let test = test::parse(test);
-
-        let throw_true = iter.next().unwrap();
-        assert!(throw_true.starts_with("    If true: throw to monkey "));
-        let throw_true = *&throw_true[29..].parse::<usize>().unwrap();
-
-        let throw_false = iter.next().unwrap();
-        assert!(throw_false.starts_with("    If false: throw to monkey "));
-        let throw_false = *&throw_false[30..].parse::<usize>().unwrap();
-
-        Self {
-            items,
-            inspections: U512::from(0),
-            op,
-            test,
-            throw_true,
-            throw_false,
-        }
-    }
+    pub id: usize,
+    pub items: VecDeque<u64>,
+    pub inspections: u64,
+    op_is_add: bool,
+    operand: Option<u64>,
+    test_factor: u64,
+    targets: (usize, usize),
 }
 
 impl Monkey {
-    /// Inspect the next item, if any, and return the item
-    /// and to which Monkey the item should be thrown.
-    pub fn inspect(&mut self, round_2: bool) -> Option<(U512, usize)> {
+    pub fn inspect_item(&mut self) -> Option<(u64, usize)> {
         let mut item = self.items.pop_front()?;
-        item = (self.op)(&item);
-        if !round_2 {
-            let (res, overflow) = item.overflowing_div(U512::from(3));
-            assert!(!overflow);
-            item = res;
-        }
-        let test = (self.test)(&item);
+        self.inspections += 1;
 
-        let (res, overflow) = self.inspections.overflowing_add(U512::from(1));
-        assert!(!overflow);
-        self.inspections = res;
-        Some((
-            item,
-            if test {
-                self.throw_true
-            } else {
-                self.throw_false
-            },
-        ))
+        item = match (self.op_is_add, self.operand) {
+            (true, None) => item + item,
+            (false, None) => item * item,
+            (true, Some(value)) => item + value,
+            (false, Some(value)) => item * value,
+        };
+        item /= 3;
+
+        let target = if item % self.test_factor == 0 {
+            self.targets.0
+        } else {
+            self.targets.1
+        };
+
+        Some((item, target))
+    }
+}
+
+impl<S: AsRef<str>> From<&[S; 6]> for Monkey {
+    fn from(lines: &[S; 6]) -> Self {
+        let mut lines = lines.iter();
+
+        let id = lines.next().unwrap().as_ref();
+        let id: usize = id[7..8].parse().unwrap();
+
+        let items = lines.next().unwrap().as_ref();
+        let (_, list) = items.split_at(18); // right after `items: `
+        let items: VecDeque<u64> = list
+            .split(", ")
+            .map(|chars| chars.parse::<u64>().unwrap())
+            .collect();
+
+        let op = lines.next().unwrap().as_ref();
+        let op = &op[23..]; // right after `= old `
+        let (operator, operand) = op.split_once(' ').unwrap();
+        let op_is_add = operator == "+";
+        let operand = if operand == "old" {
+            None
+        } else {
+            Some(operand.parse::<u64>().unwrap())
+        };
+
+        let test = lines.next().unwrap().as_ref();
+        let test = &test[21..]; // right after `divisible by `
+        let test_factor = test.parse::<u64>().unwrap();
+
+        let targets = (
+            lines.next().unwrap().as_ref(),
+            lines.next().unwrap().as_ref(),
+        );
+        let targets = (
+            targets.0[29..].parse::<usize>().unwrap(),
+            targets.1[30..].parse::<usize>().unwrap(),
+        );
+
+        assert!(lines.next().is_none());
+        Self {
+            id,
+            items,
+            inspections: 0,
+            op_is_add,
+            operand,
+            test_factor,
+            targets,
+        }
     }
 }
